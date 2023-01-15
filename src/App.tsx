0f7358876ae2ca1da980db15de6da4db5849dc80
Anthony <anthony.yc.ku@gmail.com>
2023-01-15 01:35:38 -0500

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from "@firebase/firestore";
+import { db } from './firebase-config'
+import { User } from "./models/user.model";
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const usersCollectionRef = collection(db, "users")
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      return data;
+    }
+
+    getUsers().then(response => {
+      setUsers(response.docs.map(doc => ({ ...doc.data() })) as User[]);
+    });
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {users.map((user: User) => (
+          <div>{user.name}</div>
+        ))}
+      </div>
+    </>
   );
 }
 
