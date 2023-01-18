d1ba1112eeaa3e62d33f8cc5aa25b240b58a0266
Anthony <anthony.yc.ku@gmail.com>
2023-01-18 01:20:34 -0500

    added loading animation

--- src/global/components/navigation/components/google-auth-button.tsx
+++ src/global/components/navigation/components/google-auth-button.tsx
@@ -1,17 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import GoogleLogin, { GoogleLoginResponse, GoogleLoginResponseOffline, GoogleLogout } from "react-google-login";
 import type { RootState } from '../../../../app/store';
 import { useSelector, useDispatch } from "react-redux";
 import { signIn, signOut, setUserInfo } from "../../../../features/auth/authSlice";
+import LoadingPulse from "../../loading/loading-pulse";
+import { gapi } from "gapi-script";
 
-interface authProps {
-  clientId: string;
-}
+const GoogleAuthButton = () => {
+  const clientId = '260492928179-bfugkb95ptjvit0hg8ooul8quppar8i5.apps.googleusercontent.com';
 
-const GoogleAuthButton = ({ clientId }: authProps) => {
   const { name, imageUrl } = useSelector((state: RootState) => state.auth.userInfo);
   const signedIn = useSelector((state: RootState) => state.auth.signedIn);
   const dispatch = useDispatch();
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope: ''
+      });
+      setTimeout(() => setIsLoading(false), 500);
+    };
+    gapi.load('client:auth2', initClient);
+  }, []);
 
   const onSuccess = (res: (GoogleLoginResponse | GoogleLoginResponseOffline)) => {
     const validResponse = res as GoogleLoginResponse;
@@ -30,34 +43,34 @@ const GoogleAuthButton = ({ clientId }: authProps) => {
 
   return (
     <div className="flex">
-
-      {signedIn && (
-
-        <div className="flex flex-col mr-4">
-          <p className="text-white">Signed in as:</p>
-
-          <div className="flex">
-            <img width={20} height={20} src={imageUrl} alt="" referrerPolicy="no-referrer"/>
-            <p className="text-white ml-2">{name}</p>
-          </div>
-        </div>
-      )}
+      {!signedIn && isLoading && <LoadingPulse/>}
 
       {signedIn ? (
-        <GoogleLogout
-          clientId={clientId}
-          buttonText="Log out"
-          onLogoutSuccess={logOut}
-        />
+        <>
+          <div className="flex flex-col mr-4 h-12">
+            <p className="text-white">Signed in as:</p>
+            <div className="flex">
+              <img width={20} height={20} src={imageUrl} alt="" referrerPolicy="no-referrer"/>
+              <p className="text-white ml-2">{name}</p>
+            </div>
+          </div>
+          <GoogleLogout
+            clientId={clientId}
+            buttonText="Log out"
+            onLogoutSuccess={logOut}
+          />
+        </>
       ) : (
-        <GoogleLogin
-          clientId={clientId}
-          buttonText="Sign in with Google"
-          onSuccess={onSuccess}
-          onFailure={onFailure}
-          cookiePolicy='single_host_origin'
-          isSignedIn={true}
-        />
+        <div className="h-12" style={{ display: `${!signedIn && isLoading ? 'none' : 'inline'}` }}>
+          <GoogleLogin
+            clientId={clientId}
+            buttonText="Sign in with Google"
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+            cookiePolicy='single_host_origin'
+            isSignedIn={true}
+          />
+        </div>
       )}
     </div>
   )
--- src/global/components/navigation/nav-bar.tsx
+++ src/global/components/navigation/nav-bar.tsx
@@ -16,23 +16,12 @@ const LinkButton = ({ text, path }: LinkButtonProps) => {
   )
 }
 const NavBar = () => {
-  const clientId = '260492928179-bfugkb95ptjvit0hg8ooul8quppar8i5.apps.googleusercontent.com';
-
-  useEffect(() => {
-    const initClient = () => {
-      gapi.client.init({
-        clientId: clientId,
-        scope: ''
-      });
-    };
-    gapi.load('client:auth2', initClient);
-  }, []);
 
   return (
     <>
       <nav className="flex bg-gray-900 w-full text-white px-4 py-2 justify-between items-center">
         <h1 className="h-6 mr-3 sm:h-9 font-bold text-lg">IOU Keeper</h1>
-        <GoogleAuthButton clientId={clientId}/>
+        <GoogleAuthButton/>
       </nav>
       <nav className="bg-gray-700">
         <ul className="flex flex-row mr-6 font-medium space-x-8 px-2">
