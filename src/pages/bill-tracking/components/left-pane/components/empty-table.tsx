05a39c919fd265eda068d025cc1e8a0eb23e11fe
Anthony <anthony.yc.ku@gmail.com>
2023-02-04 18:52:26 -0500

    updated button create

--- src/pages/bill-tracking/components/left-pane/components/empty-table.tsx
+++ src/pages/bill-tracking/components/left-pane/components/empty-table.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import CreateButton from "../../../../../global/components/buttons/create-button";
+import UseDialogHook from "../../../../../global/components/dialog/hooks/use-dialog-hook";
+import DialogSpinningLoader from "../../../../../global/components/loading/dialog-spinning-loader";
+import CreateEditDebtDialog from "../../../dialog/create-edit-debt-dialog";
 
 const EmptyTable = () => {
 
-  const handleCreateButton = () => {
-  }
+  const { modalRef, setOpenDialog, openDialog, animationClass } = UseDialogHook();
 
   return (
     <div className="flex flex-col justify-center items-center h-full text-gray-200 space-y-2">
+      <Suspense fallback={<DialogSpinningLoader/>}>
+        {openDialog &&
+            <CreateEditDebtDialog animationClass={animationClass} modalRef={modalRef} setOpenDialog={setOpenDialog}/>}
+      </Suspense>
+
+
       <i className="fa fa-info-circle" style={{ fontSize: '3rem' }}/>
       <p className="font-medium">There is no data to display</p>
 
-      <CreateButton handleClick={handleCreateButton} text="Create new debt"/>
+      <CreateButton handleClick={() => setOpenDialog(true)} text="Create new debt"/>
     </div>
   )
 };
