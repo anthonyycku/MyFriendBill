cc5006f998317bb8c0b858f81b74b68fa409d343
Anthony <anthony.yc.ku@gmail.com>
2023-02-08 01:40:24 -0500

    added guest sign-in and about

--- src/pages/landing-page/landing-page.tsx
+++ src/pages/landing-page/landing-page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import GoogleSignInButton from "./components/google-sign-in-button";
 import GuestSignInButton from "./components/guest-sign-in-button";
 import AboutButton from "./components/about-button";
+import Divider from "../../global/components/divider/divider";
 
 const LandingPage = () => {
   return (
@@ -10,13 +11,14 @@ const LandingPage = () => {
         className="flex flex-col justify-center h-96 w-[500px] p-6 shadow-[1px_1px_3px_0] shadow-lime-800 bg-gray-800 hover:bg-gray-700 rounded">
         <div className="flex flex-col justify-center items-center space-y-2">
           <GoogleSignInButton/>
-          <GuestSignInButton/>
-        </div>
-        <div className="flex flex-col my-6 px-10 space-y-2">
-          <p>Your Google information will<span className="font-medium">{` NOT `}</span> be collected.</p>
           <p>Sign in with Google for a better experience.</p>
+          <p>Your Google information will<span className="font-medium">{` NOT `}</span> be collected.</p>
+          <Divider/>
+          <div className="mt-4"/>
+          <GuestSignInButton/>
           <p>Guest sign-in creates a new guest user each time.</p>
         </div>
+        <Divider/>
         <div className="flex justify-center">
           <AboutButton/>
         </div>
